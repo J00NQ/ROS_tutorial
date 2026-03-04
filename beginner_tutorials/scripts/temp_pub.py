4132b51bd3be9d72e9308141368104ee4354bc50
J00NQ <kjk020913@gmail.com>
2026-03-03 23:39:31 -0800

    feat : temp_sub.py

--- beginner_tutorials/scripts/temp_pub.py
+++ beginner_tutorials/scripts/temp_pub.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 
 import rospy
-from std_msgs.msg import Int32
+from std_msgs.msg import Float32
 import random
 
 def temperature():
-    pub = rospy.Publisher('temperature', Int32, queue_size=10)
+    pub = rospy.Publisher('temperature', Float32, queue_size=10)
     rospy.init_node('temperature_pub')
     rate = rospy.Rate(1)  # 1Hz = 1초에 1번
-    temp = 0
+    temp = random.uniform(20.0, 40.0)
 
     while not rospy.is_shutdown():
-        rospy.loginfo("온도 발행: %d", temp)
+        rospy.loginfo("온도 발행: %.1f도", temp)
         pub.publish(temp)
         temp = random.uniform(20.0, 40.0)
         rate.sleep()
